30f7b5f74f430496c2fdf9ae4975abb2185dee78
php_yang <1034211002@qq.com>
2019-11-15 09:55:23 +0800

    修改文件上传大小限制

--- src/Upload.php
+++ src/Upload.php
@@ -11,19 +11,24 @@ class Upload
 {
     public function upload(UploadedFile $file)
     {
-        if (!$file->isValid()) {
-            throw new UploadException('文件验证失败');
-        }
         try {
             $default = config('upload.default');
             $config = config('upload.drivers')[$default];
             $allow_ext = $config['allowed_ext'];
+            $upload_max_size = $config['upload_max_size'];
             $ext = $file->extension();
+            $size = round($file->getSize() / 1024 / 1024, 2);
+            if ($size > $upload_max_size) {
+                throw new UploadException($size . 'M超过最大允许值');
+            }
             if (!in_array($ext, $allow_ext)) {
                 throw new UploadException($ext . '后缀不允许');
             }
         } catch (\Exception $exception) {
             throw $exception;
+        }
+        if (!$file->isValid()) {
+            throw new UploadException('文件验证失败');
         }
         $etg = Etag::sum($file->getPath() . '/' . $file->getFilename());
         $key = $etg[0] . '.' . $ext;
